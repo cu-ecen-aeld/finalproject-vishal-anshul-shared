0360b4ee4f6e74ecaae4784d525130a419d2975f
vishalraj3112 <vishal.raj15@siesgst.ac.in>
2022-04-23 18:58:18 -0600

    bbb ip added in client

--- client/client.c
+++ client/client.c
@@ -363,7 +363,7 @@ int setup_comm()
 	memset(&servaddr, 0, sizeof(servaddr));
 	
 	/* Assign IP and port */
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_addr.s_addr = inet_addr("10.0.0.36");
 	servaddr.sin_family = AF_INET;  
 	servaddr.sin_port = htons(PORT);
 	
@@ -382,7 +382,7 @@ int setup_comm()
 
 int main()
 {
-	printf("***Starting client program Version 127.0...***\n");
+	printf("***Starting client program Version GPIO***\n");
 
 	/*Start syslog daemon*/
     openlog("client", LOG_USER, LOG_DEBUG|LOG_ERR); 
